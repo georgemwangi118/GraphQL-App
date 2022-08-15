b805e9d7255b544ca731025ebbf74e84756124d4
georgemwangi118 <gmwangi118@yahoo.com>
2022-08-15 22:16:18 +0300

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import Category from "./component/Category/Category";
 import Navbar from "./component/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Navbar />
-        <Category />
+        <Routes>
+          <Route path="/" element={<Category />} />
+        </Routes>
       </>
     );
   }
